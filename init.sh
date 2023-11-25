2e476f6bd88b73303a9c9f56f2a696024df980e5
Ryan Susana <ryansusana97@gmail.com>
2023-11-25 14:25:23 +0100

    cleanup

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 export PATH=$PATH:"$WORKSPACE_MANAGER/scripts"
-export CDPATH=$CDPATH:$WORKSPACE_MANAGER
-alias cdp='cd $(selectp)'
-alias cdd='cd $(selectdd)'
-alias opend='cd $(selectd)'
-alias opendd='cd $(selectdd)'
+export CDPATH=$CDPATH:$WORKSPACE_MANAGER:"$WORKSPACE_MANAGER/workspaces"
+alias cdw="cd $WORKSPACE_MANAGER/workspaces"
+alias cdp='project=$(selectp) && cd $project'
+alias cdd='dir=$(selectdd) && cd $dir'
+alias opend='dir=$(selectd)  && cd $dir'
+alias openf='file=$(selectf) && vim $file'
+alias openff='file=$(selectff) && vim $file'
+alias openp='project=$(selectp) && open_editor $project'
