8269ae718338f5d0f8db2b75fe1f36b78e148201
Ryan Susana <ryansusana97@gmail.com>
2023-11-26 14:18:35 +0100

    added opendd

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ export CDPATH=$CDPATH:$WORKSPACE_MANAGER:"$WORKSPACE_MANAGER/workspaces"
 alias cdw="cd $WORKSPACE_MANAGER/workspaces"
 alias cdp='project=$(selectp) && cd $project'
 alias cdd='dir=$(selectdd) && cd $dir'
+alias opendd='dir=$(selectdd) && cd $dir'
 alias opend='dir=$(selectd)  && cd $dir'
 alias openf='file=$(selectf) && vim $file'
 alias openff='file=$(selectff) && vim $file'
