18cf4979bc322b94a9111b9c2991e0f7b41eb745
Ryan Susana <ryansusana97@gmail.com>
2023-11-27 10:52:15 +0100

    added nvim support :)

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 export PATH=$PATH:"$WORKSPACE_MANAGER/scripts"
 export CDPATH=$CDPATH:$WORKSPACE_MANAGER:"$WORKSPACE_MANAGER/workspaces"
+alias editf='e(){nvim $1 || vim $1 || vi $1 || nano $1}; e'
 alias cdw="cd $WORKSPACE_MANAGER/workspaces"
 alias cdp='project=$(selectp) && cd $project'
 alias cdd='dir=$(selectdd) && cd $dir'
 alias opendd='dir=$(selectdd) && cd $dir'
 alias opend='dir=$(selectd)  && cd $dir'
-alias openf='file=$(selectf) && vim $file'
-alias openff='file=$(selectff) && vim $file'
+alias openf='file=$(selectf) && editf $file'
+alias openff='file=$(selectff) && editf $file'
 alias openp='project_path=$(selectp) && project=$(echo "$project_path" | awk -F/ "{print \$(NF-2)\"/\"\$(NF-1)}"); tmux attach -t "$project" || tmux new -s "$project" -c "$project_path"'
 
